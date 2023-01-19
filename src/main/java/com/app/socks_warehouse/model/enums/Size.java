66b2548c64a38958faad14c1e683aee74a710aac
ALEXDRUGS <105850199+ALEXDRUGS@users.noreply.github.com>
2023-01-19 15:02:38 +0300

    getquantity

--- src/main/java/com/app/socks_warehouse/model/enums/Size.java
+++ src/main/java/com/app/socks_warehouse/model/enums/Size.java
@@ -1,11 +1,14 @@
 package com.app.socks_warehouse.model.enums;
 
+import com.fasterxml.jackson.annotation.JsonValue;
+
 public enum Size {
     L("34"),
     S("36"),
     M("38"),
     XL("42"),
     XXL("45");
+    @JsonValue
     private final String size;
 
     Size(String size) {
